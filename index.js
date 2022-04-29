6eccd7457a2266b991862a4030f24314d3e258b9
Vishnuprasad K <31353786+vishnuprasadk0123@users.noreply.github.com>
2022-04-29 18:53:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,6 +72,6 @@ app.get("/test2", async function (req, res) {
   }
 });
 
-app.listen(3000, function () {
-  console.log("Listening on port 3000...");
+app.listen(80, function () {
+  console.log("Listening on port 80...");
 });
